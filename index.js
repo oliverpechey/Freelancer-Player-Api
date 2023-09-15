8a7f9ed09411704fe1991a843f77fb9afa87fd2e
oliverpechey <olliezeta9@gmail.com>
2023-09-15 18:56:39 +0100

    Fix all and add /player

--- index.js
+++ index.js
@@ -11,19 +11,25 @@ let players = {};
 
 // /all GET request
 router.get('/all', (req, res) => {
-    res.json(players);
+    let response = Object.fromEntries(players);
+    res.json(response);
 });
 
+// /player GET request
+router.get('/player/:player', (req, res) => {
+    res.json(players.get(req.params.player));
+});
 
 // Uses the freeelancer-save-parser module to parse the player files
 const parsePlayers = () => {
-    players = playerParser.Parser()
+    let unfiledPlayers = new playerParser.Parser()
         .ParsePlayerFiles(process.env.SAVEPATH)
         .SortPlayerFiles('LastSeen','Desc')
         .players;
+    players = new Map(unfiledPlayers.map(element => [element.name, element]));
 }
 
 app.use('/', router);
 app.listen(3000);
-
+parsePlayers();
 setInterval(parsePlayers, process.env.INTERVAL * 1000);
